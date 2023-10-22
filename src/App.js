c724994d03fc87fb4a8a45e9e56c7026c8c3dc15
Patkav <patricia.kavanaugha@gmail.com>
2023-10-21 21:57:24 -0400

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
@@ -5,7 +7,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
-
+        <Weather />
         <footer>
           This project was coded by Patricia Kavanaugh and is{" "}
           <a
