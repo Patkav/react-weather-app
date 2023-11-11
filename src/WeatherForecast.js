6c3bd498890600a3fae8e913b1da7f45f152430f
Patkav <patricia.kavanaugha@gmail.com>
2023-11-10 22:03:23 -0500

    Updated weatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -20,7 +20,7 @@ export default function WeatherForecast(props) {
     let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let longitude = props.data.lon;
     let latitude = props.data.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
   }
